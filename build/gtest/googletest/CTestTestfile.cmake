10c37ad2da0579458dcdbec43ce34b39b8c552f1
Ahmed Zermane <ahmed.zermane@lirmm.fr>
2023-07-04 09:32:51 +0200

    remove build f

--- build/gtest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/googletest/googletest
-# Build directory: /home/ahmedaymen/WorkSpace/mc_rtc/src/catkin_data_ws/src/mc_rtc_data/mc_go1_description/build/gtest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
